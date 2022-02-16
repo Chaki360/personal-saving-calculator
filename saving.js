e5f09bfcdef8f5c250c0bf8ddd7dd8687220eef5
Badhon chaki <fboy4069@gmail.com>
2022-02-17 05:19:49 +0600

    css commited

---
+++ saving.js
@@ -0,0 +1,21 @@
+
+
+const totalCost = document.getElementById("calculator").addEventListener('click', function () {
+
+    const incomeInput = document.getElementById('income-input').value;
+    const totalIncome = parseInt(incomeInput);
+
+
+
+    const foodInput = document.getElementById('food-input').value;
+    const totalFoodCost = parseInt(foodInput);
+
+    const rentInput = document.getElementById('rent-input').value;
+    const totalRentCost = parseInt(rentInput);
+
+    const ClothInput = document.getElementById('cloth-input').value;
+    const totalClothCost = parseInt(ClothInput);
+
+
+
+})
